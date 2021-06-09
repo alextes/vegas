60c18b67ab92b08c0b1a2b38d493f3a9ccec8509
cryptogohan <cryptogohan@protonmail.com>
2021-06-09 15:06:58 +0200

    Add v1 of vegas
    Adds randomInt, randomBelow, randomPick, randomSample, and randomFloat.
    Adds tests.
    Adds documentation.
    Adds vendored seeded random number generation library that sadly is not
    ESM compatible.

---
+++ test.ts
@@ -0,0 +1,32 @@
+import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
+import { makeSeededGenerators } from "./mod.ts";
+
+const vegas = makeSeededGenerators("test-seed");
+
+Deno.test("random int", () => {
+  assertEquals(vegas.randomInt(0, 4), 1);
+  assertEquals(vegas.randomInt(0, 4), 2);
+  assertEquals(vegas.randomInt(0, 4), 2);
+  assertEquals(vegas.randomInt(0, 4), 1);
+  assertEquals(vegas.randomInt(0, 4), 1);
+  assertEquals(vegas.randomInt(0, 4), 1);
+  assertEquals(vegas.randomInt(0, 4), 0);
+  assertEquals(vegas.randomInt(0, 4), 1);
+  assertEquals(vegas.randomInt(0, 4), 3);
+});
+
+Deno.test("pick random element", () => {
+  assertEquals(vegas.randomPick([1, 2, 3]), 3);
+});
+
+Deno.test("random int below", () => {
+  assertEquals(vegas.randomBelow(8), 1);
+});
+
+Deno.test("random float", () => {
+  assertEquals(vegas.randomFloat(), 0.6410711158367218);
+});
+
+Deno.test("random sample", () => {
+  assertEquals(vegas.randomSample([1, 2, 3, 4], 2), [4, 1]);
+});
