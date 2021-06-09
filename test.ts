08d6ba4b727a137602c58911d7dd04f35a2406b9
cryptogohan <cryptogohan@protonmail.com>
2021-06-09 15:53:39 +0200

    Make test descriptions more descriptive

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@ import { makeSeededGenerators } from "./mod.ts";
 
 const vegas = makeSeededGenerators("test-seed");
 
-Deno.test("random int", () => {
+Deno.test("generate random int", () => {
   assertEquals(vegas.randomInt(0, 4), 1);
   assertEquals(vegas.randomInt(0, 4), 2);
   assertEquals(vegas.randomInt(0, 4), 2);
@@ -19,14 +19,14 @@ Deno.test("pick random element", () => {
   assertEquals(vegas.randomPick([1, 2, 3]), 3);
 });
 
-Deno.test("random int below", () => {
+Deno.test("generate random int below", () => {
   assertEquals(vegas.randomBelow(8), 1);
 });
 
-Deno.test("random float", () => {
+Deno.test("generate random float", () => {
   assertEquals(vegas.randomFloat(), 0.6410711158367218);
 });
 
-Deno.test("random sample", () => {
+Deno.test("pick random sample", () => {
   assertEquals(vegas.randomSample([1, 2, 3, 4], 2), [4, 1]);
 });
